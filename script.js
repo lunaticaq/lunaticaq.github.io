af12af2f7422d7b953a2f12fa7d2522704e68b8d
Arthur <84716995+arthur875@users.noreply.github.com>
2025-05-07 13:16:28 +0200

    added scoller to the index page with the corresponding style and script

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const scrollContainer = document.querySelector('.scroll-container');
+    scrollContainer.addEventListener('wheel', (evt) => {
+        evt.preventDefault();
+        scrollContainer.scrollLeft += evt.deltaY;
+    });
+});
